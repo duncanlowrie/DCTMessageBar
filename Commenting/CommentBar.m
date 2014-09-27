43dec188073ee87a348a6a5773e540cc4672f41d
Daniel Tull <dt@danieltull.co.uk>
2014-09-27 13:03:18 +0100

    Simplify demo, but remove resizing

--- Commenting/CommentBar.m
+++ Commenting/CommentBar.m
@@ -20,59 +20,46 @@
 
 #pragma mark - UIView
 
-- (BOOL)translatesAutoresizingMaskIntoConstraints {
-	return NO;
+- (void)didMoveToSuperview {
+	[super didMoveToSuperview];
+	[self updatePlaceholderAlpha];
+	[self setNeedsLayout];
+	[self layoutIfNeeded];
 }
 
-// To force the view to get a contentScaleFactor
-- (void)drawRect:(CGRect)rect {
-	[super drawRect:rect];
+- (void)didMoveToWindow {
+	[super didMoveToWindow];
+	self.textView.layer.borderWidth = 1.0f/self.window.screen.nativeScale;
 }
 
 - (void)layoutSubviews {
+	CGSize currentSize = self.frame.size;
+	CGFloat sendWidth = [self.sendButton intrinsicContentSize].width;
+	CGFloat totalMargins = [[self.marginConstraints valueForKeyPath:@"@sum.constant"] floatValue];
+	self.textView.preferredMaxLayoutWidth = currentSize.width - totalMargins - sendWidth;
 	[super layoutSubviews];
-	[self calculateFrame];
 }
 
 #pragma mark - CommentBar
 
-- (void)calculateFrame {
-
-	CGSize currentSize = self.frame.size;
-
-	CGFloat sendWidth = [self.sendButton intrinsicContentSize].width;
-	CGFloat totalMargins = [[self.marginConstraints valueForKeyPath:@"@sum.constant"] floatValue];
-	self.textView.preferredMaxLayoutWidth = currentSize.width - totalMargins - sendWidth;
-
-	CGSize targetSize = CGSizeMake(currentSize.width, 0);
-	CGSize size = [self systemLayoutSizeFittingSize:targetSize];
-	CGRect frame = self.frame;
-	frame.origin.y += CGRectGetHeight(frame) - size.height;
-	frame.size.height = size.height;
-	self.frame = frame;
-	[self layoutIfNeeded];
-}
 
 - (void)setTextView:(IntrinsicTextView *)textView {
 	_textView = textView;
 	_textView.layer.cornerRadius = 6.0f;
 	_textView.layer.borderColor = [[UIColor colorWithWhite:0.8f alpha:1.0f] CGColor];
-	_textView.layer.borderWidth = 1.0f/self.contentScaleFactor;
 }
 
 - (void)updatePlaceholderAlpha {
 	BOOL hidden = self.textView.text.length > 0;
 	CGFloat alpha = hidden ? 0.0f : 1.0f;
 	self.placeholder.alpha = alpha;
+	self.sendButton.enabled = hidden;
 }
 
 #pragma mark - UITextViewDelegate
 
 - (void)textViewDidChange:(UITextView *)textView {
-	[self invalidateIntrinsicContentSize];
-	[textView invalidateIntrinsicContentSize];
 	[self updatePlaceholderAlpha];
-	[self calculateFrame];
 }
 
 @end
